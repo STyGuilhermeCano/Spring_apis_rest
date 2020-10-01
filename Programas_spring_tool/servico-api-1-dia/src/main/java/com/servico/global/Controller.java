a9a05793e65a4e09de1ee102b5a19c6a5ecb854c
unknown <guilherme.cano@outlook.com>
2020-10-01 13:49:51 -0300

    Primeira Api desenvolvida em aula

---
+++ Programas_spring_tool/servico-api-1-dia/src/main/java/com/servico/global/Controller.java
@@ -0,0 +1,31 @@
+package com.servico.global; // post - get - delete - update=put this class is my CRUD
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@Autowired
+	private ServicoRepository repository;
+	
+	@GetMapping("/servicos")
+	public List<ServicoModel> pegarTodos() { // list eta fazendo o papel do select * from pegar todos é o metodo
+		
+		return repository.findAll(); // get
+	}
+	// requerer a resposta save
+	
+	@PostMapping("/servicos")
+	public ServicoModel criar(@RequestBody ServicoModel model) {
+		repository.save(model);
+		return model;
+	}
+	
+	
+}
