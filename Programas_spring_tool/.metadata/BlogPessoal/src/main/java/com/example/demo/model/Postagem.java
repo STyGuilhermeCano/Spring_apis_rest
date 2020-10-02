17902865c947b1f041b471037d7239b071756bb9
unknown <guilherme.cano@outlook.com>
2020-10-02 12:38:51 -0300

    exercicio Crud GET POST PUT DELETE

---
+++ Programas_spring_tool/.metadata/BlogPessoal/src/main/java/com/example/demo/model/Postagem.java
@@ -0,0 +1,75 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.sun.istack.NotNull;
+
+
+
+@Entity
+@Table(name = "postagem")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	// nao fiz o Size
+	private String titulo;
+	
+	@NotNull
+	// nao fiz o Size
+	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+
+	//getters and setters
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	};
+	
+	
+	
+	
+	
+	
+}
