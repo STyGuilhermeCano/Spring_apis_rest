2253979965e955716a9bd94ad7c978f5b8f46898
unknown <guilherme.cano@outlook.com>
2020-11-12 00:27:05 -0300

    projeto atualizad rodando com o front

--- Programas_spring_tool/.metadata/BlogPessoal/src/main/java/com/example/demo/controller/TemaController.java
+++ Programas_spring_tool/.metadata/BlogPessoal/src/main/java/com/example/demo/controller/TemaController.java
@@ -32,7 +32,7 @@ public class TemaController {
 		
 	}
 	
-	@GetMapping("/{Id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Tema> getById(@PathVariable long id){
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
